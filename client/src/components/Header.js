2c1e7be2767cd6637ec5721cab416bca5465c83c
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-07-30 20:35:09 +0700

    [client] Refactoring code.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { Row, Col, Nav, InputGroup, FormControl } from 'react-bootstrap'
 import logo from '../assets/pokemon.svg'
+import Icon from './atoms/Icons'
 
 const Header = () => {
   return (
     <div className='Header'>
       <Row>
         <Col xs lg="2">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Row className='pl-5'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2 className='ml-2'>Pokedx</h2>
+          </Row>
         </Col>
         <Col xs lg="8">
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </InputGroup>
+          <Icon
+            name='search'
+            size={30}
+            color='#576574'
+          />
         </Col>
       </Row>
     </div>
--- client/src/components/atoms/Icons.js
+++ client/src/components/atoms/Icons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Search as SearchLogo } from '../../assets/search.svg'
+import { SearchIcon } from '../../helpers/icons'
 
 class Icon extends React.Component {
   getPath() {
@@ -7,10 +7,7 @@ class Icon extends React.Component {
     switch (name) {
       case 'search':
         return (
-          <path d="M447.05,428l-109.6-109.6c29.4-33.8,47.2-77.9,47.2-126.1C384.65,86.2,298.35,0,192.35,0C86.25,0,0.05,86.3,0.05,192.3
-            s86.3,192.3,192.3,192.3c48.2,0,92.3-17.8,126.1-47.2L428.05,447c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4
-            C452.25,441.8,452.25,433.2,447.05,428z M26.95,192.3c0-91.2,74.2-165.3,165.3-165.3c91.2,0,165.3,74.2,165.3,165.3
-            s-74.1,165.4-165.3,165.4C101.15,357.7,26.95,283.5,26.95,192.3z" fill={color} />
+          <SearchIcon fill={color}/>
         )  
       default:
         break;
@@ -18,7 +15,7 @@ class Icon extends React.Component {
   }
 
   render() {
-    const { style, viewBox, className, size } = this.props
+    const { size } = this.props
     return (
       <svg
         version="1.1"
