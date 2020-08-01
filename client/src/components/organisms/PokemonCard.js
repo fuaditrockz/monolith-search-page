1e4512deba56c555c6ec993599e6f242cb25e190
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-01 18:16:00 +0700

    Some update

---
+++ client/src/components/organisms/PokemonCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  Col,
+  Row
+} from 'react-bootstrap'
+
+class PokemonCard extends React.Component {
+  render() {
+    return (
+      <Col
+        className='pokemon-card'
+        md={4}
+        style={{
+          backgroundColor: 'black'
+        }}
+      >
+        
+      </Col>
+    )
+  }
+}
+
+export default PokemonCard
--- client/src/containers/HomeScreen.js
+++ client/src/containers/HomeScreen.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import {
-  Container,
-  Col,
-  Row,
-  Card,
-  Button,
-  Nav
-} from 'react-bootstrap'
+import { Container, Row, } from 'react-bootstrap'
+
 import Header from '../components/organisms/Header'
+import PokemonCard from '../components/organisms/PokemonCard'
 
 class HomeScreen extends React.PureComponent {
   render() {
@@ -16,19 +11,9 @@ class HomeScreen extends React.PureComponent {
         <Header />
         <Container fluid>
           <Row>
-            <Col>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://i.ytimg.com/vi/L4y_C4Ik92c/maxresdefault.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
+            <PokemonCard />
+            <PokemonCard />
+            <PokemonCard />
           </Row>
         </Container>
       </>
