20defd1c56e8cd319ab85cb72985f4060fdc42a0
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-01 19:09:19 +0700

    [client] Finishing PokemonCard component.

--- client/src/components/organisms/PokemonCard.js
+++ client/src/components/organisms/PokemonCard.js
@@ -9,12 +9,27 @@ class PokemonCard extends React.Component {
     return (
       <Col
         className='pokemon-card'
-        md={4}
-        style={{
-          backgroundColor: 'black'
-        }}
+        md={3}
       >
-        
+        <Row className='pl-3'>
+          <Col>
+            <h3>LAPRAS</h3>
+            <h5>#131</h5>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='pokemon-image'>
+            <img src='https://cdn.bulbagarden.net/upload/7/73/004Charmander.png' width={200} />
+          </Col>
+        </Row>
+        <Row className='pl-3 pr-3 pokemon-card-footer'>
+          <Col md={6} className='pokemon-type'>
+            <h5>Water</h5>
+          </Col>
+          <Col md={6} className='pokemon-sign'>
+            <h5>Pokemon</h5>
+          </Col>
+        </Row>
       </Col>
     )
   }
