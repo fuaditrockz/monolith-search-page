c545b0cacbbbb85471c75adfb6d6ce4394dd17d3
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-03 00:38:44 +0700

    [server] Create responser.

---
+++ server/helpers/responsers.js
@@ -0,0 +1,36 @@
+exports.GET_RESPONSE = {
+  success: (isSingleItem, payloadName, payload) => {
+    return {
+      error: false,
+      statusCode: 200,
+      message: `Success GET ${isSingleItem ? '' : 'all'} ${payloadName}`,
+      total: isSingleItem ? 1 : payload.length,
+      data: isSingleItem ? payload : [payload]
+    }
+  },
+  error: err => {
+    return {
+      error: true,
+      statusCode: 404,
+      message: err
+    }
+  }
+}
+
+exports.POST_RESPONSE = {
+  success: (payload) => {
+    return {
+      error: false,
+      statusCode: 201,
+      message: `${payload.name} was created successfully`,
+      objectId: payload._id
+    }
+  },
+  error: err => {
+    return {
+      error: true,
+      statusCode: 400,
+      message: err
+    }
+  }
+}
