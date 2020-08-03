d1e5ddde34ac8806debf55d0eb56b61502b712ba
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-03 16:12:17 +0700

    [client] Setup react-router-dom.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Container, Row, } from 'react-bootstrap'
+import Header from './components/organisms/Header'
+
 import HomeScreen from './containers/HomeScreen'
 
 function App() {
   return (
-    <HomeScreen />
+    <Router>
+      <Container fluid>
+        <Header />
+      </Container>
+      <Switch>
+        <Route path='/'>
+          <HomeScreen />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/containers/HomeScreen.js
+++ client/src/containers/HomeScreen.js
@@ -1,30 +1,24 @@
 import React from 'react'
 import { Container, Row, } from 'react-bootstrap'
 
-import Header from '../components/organisms/Header'
 import PokemonCard from '../components/organisms/PokemonCard'
 
 class HomeScreen extends React.PureComponent {
   render() {
     return (
-      <>
-        <Container fluid>
-          <Header />
-        </Container>
-        <Container fluid>
-          <Row>
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-          </Row>
-        </Container>
-      </>
+      <Container fluid>
+        <Row>
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+        </Row>
+      </Container>
     )
   }
 }
