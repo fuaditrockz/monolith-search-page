4c287ba0f3e597baae8633cb18822854d9543f3e
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-03 20:57:39 +0700

    [client] Add icon for Pokemon type.

---
+++ client/src/constants/index.js
@@ -0,0 +1,1 @@
+export { default as POKEMON_TYPE } from './pokemonType'
---
+++ client/src/constants/pokemonType.js
@@ -0,0 +1,80 @@
+const POKEMON_TYPE = [
+  {
+    name: 'Fire',
+    icon: require('../assets/pokemonTypeIcons/fire.svg')
+  },
+  {
+    name: 'Water',
+    icon: require('../assets/pokemonTypeIcons/water.svg')
+  },
+  {
+    name: 'Rock',
+    icon: require('../assets/pokemonTypeIcons/rock.svg')
+  },
+  {
+    name: 'Normal',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  },
+  {
+    name: 'Fighting',
+    icon: require('../assets/pokemonTypeIcons/fighting.svg')
+  },
+  {
+    name: 'Flying',
+    icon: require('../assets/pokemonTypeIcons/flying.svg')
+  },
+  {
+    name: 'Grass',
+    icon: require('../assets/pokemonTypeIcons/grass.svg')
+  },
+  {
+    name: 'Poison',
+    icon: require('../assets/pokemonTypeIcons/poison.svg')
+  },
+  {
+    name: 'Electric',
+    icon: require('../assets/pokemonTypeIcons/electric.svg')
+  },
+  {
+    name: 'Ground',
+    icon: require('../assets/pokemonTypeIcons/ground.svg')
+  },
+  {
+    name: 'Psychic',
+    icon: require('../assets/pokemonTypeIcons/psychic.svg')
+  },
+  {
+    name: 'Ice',
+    icon: require('../assets/pokemonTypeIcons/ice.svg')
+  },
+  {
+    name: 'Bug',
+    icon: require('../assets/pokemonTypeIcons/bug.svg')
+  },
+  {
+    name: 'Dragon',
+    icon: require('../assets/pokemonTypeIcons/dragon.svg')
+  },
+  {
+    name: 'Ghost',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  },
+  {
+    name: 'Dark',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  },
+  {
+    name: 'Steel',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  },
+  {
+    name: 'Fairy',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  },
+  {
+    name: 'Unknown',
+    icon: require('../assets/pokemonTypeIcons/normal.svg')
+  }
+]
+
+export default POKEMON_TYPE
