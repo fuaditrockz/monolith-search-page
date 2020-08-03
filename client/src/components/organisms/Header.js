d3fa0801b30516871f97295c828ec1a9c4c95956
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-03 16:12:58 +0700

    [client] Implement Link from react-router-dom and change style decoration for anchor.

--- client/src/components/organisms/Header.js
+++ client/src/components/organisms/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Row, Col, Nav, InputGroup, FormControl } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
 import logo from '../../assets/pokemon.svg'
 import HeaderIcon from '../molecules/HeaderIcon'
 
@@ -8,10 +9,12 @@ const Header = () => {
     <div className='header'>
       <Row>
         <Col xs lg="2" className='logo-wrapper'>
-          <Row className='pl-5'>
-            <img src={logo} className="logo" alt="logo" />
-            <h2 className='ml-2'>Pokedex</h2>
-          </Row>
+          <Link to='/'>
+            <Row className='pl-5'>
+              <img src={logo} className="logo" alt="logo" />
+              <h2 className='pokedex-title ml-2'>Pokedex</h2>
+            </Row>
+          </Link>
         </Col>
         <Col xs lg="10" className='icons-wrapper'>
           <Row className='pr-5'>
