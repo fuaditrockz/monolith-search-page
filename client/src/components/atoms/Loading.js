f39763b61896fe27a31e38aa07c54e16b72023e9
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-08 00:52:22 +0700

    [client] Update Loading component.

--- client/src/components/atoms/Loading.js
+++ client/src/components/atoms/Loading.js
@@ -4,7 +4,7 @@ import loadingGif from '../../assets/loading.gif'
 const Loading = () => {
   return (
     <div className='loading-container'>
-      <img src={loadingGif} alt='loading' width={150} />
+      <img src={loadingGif} alt='loading' width={200} />
       <h4 className='mt-3'>Loading...</h4>
     </div>
   )
