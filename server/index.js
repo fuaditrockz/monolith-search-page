a5dc13c46538ee5eaa9caf8a96bf5e5667d5d5a8
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-07 22:27:25 +0700

    [server] Setup http-proxy-middleware.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const mongoConnection = require('./config/mongodb-connection')
 mongoConnection(process.env.ENVIRONMENT)
@@ -8,15 +9,19 @@ mongoConnection(process.env.ENVIRONMENT)
 const pokemonRouters = require('./routes/pokemon')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+/* app.use('/v1', createProxyMiddleware({
+  target: 'http://localhost:8080',
+  changeOrigin: true
+})) */
 app.use('/v1', pokemonRouters)
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.send({ express: 'Hello From Express' })
 });
 
 app.post('/api/world', (req, res) => {
@@ -26,4 +31,4 @@ app.post('/api/world', (req, res) => {
   );
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
